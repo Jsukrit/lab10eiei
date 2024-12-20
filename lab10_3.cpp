5816bc98c9a9e621dff2fa06cc326ec81ab794b3
Jsukrit <sukritdn1@gmail.com>
2024-12-20 16:50:12 +0700

    finished

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream source;
+	source.open("score.txt") ;
+    string count;
+    int i;
+    float Avg,SD;
+    while(getline(source,count)){
+        i++;
+       Avg += atof(count.c_str());
+        SD +=  atof(count.c_str())*atof(count.c_str());
+    }
+
+    SD = sqrt((SD/i)-(Avg/i)*(Avg/i));
+    cout << "Number of data = " << i << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << Avg/i << "\n";
+    cout << "Standard deviation = " << SD;
+}
